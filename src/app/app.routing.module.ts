0f0951d383ea1cedeaf67b381b80af5e7e707e5e
IDGS-901-19002228 <19002228@alumnos.utleon.edu.mx>
2023-06-29 00:17:01 -0600

    CFLP Clase 28/06/23

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,11 +2,17 @@ import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
 import { IricComponent } from "./grupos/iric/iric.component";
 import { OperasBasComponent } from "./grupos/formularios/operas-bas/operas-bas.component";
+import { HomeComponent } from "./home/home.component";
+import { AlumnoReactiveComponent } from "./formularios/alumno-reactive/alumno-reactive.component";
+import { SumaComponent } from "./grupos/formularios/suma/suma.component";
 
 const routes:Routes=[
     {path: '',redirectTo: '/home', pathMatch:'full'},
+    {path: 'home', component: HomeComponent},
     {path: 'IricComponent', component: IricComponent },
     {path: 'OperasBasComponent', component: OperasBasComponent },
+    {path: 'AlumnoReactiveComponent', component: AlumnoReactiveComponent },
+    {path: 'SumaComponent', component: SumaComponent },
 ];
 
 @NgModule({
--- src/app/formularios/alumno-reactive/alumno-reactive.component.ts
+++ src/app/formularios/alumno-reactive/alumno-reactive.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormControl, FormGroup, Validator, Validators } from '@ang
 export class AlumnoReactiveComponent {
 
   alumnoForm!: FormGroup;
+  alumnos: any[] = [];
 
   constructor(private readonly fb: FormBuilder) {
 
@@ -22,16 +23,29 @@ export class AlumnoReactiveComponent {
   }
 
   obtenerDatos():void{
-    const mat = this.alumnoForm.get('matricula')?.value
+
+    /*const mat = this.alumnoForm.get('matricula')?.value
     console.log('matricula',mat)
     const nom = this.alumnoForm.get('nombre')?.value
-    console.log('nombre',nom)
+    console.log('nombre',nom)*/
+
+    const alumno = {
+      matricula: this.alumnoForm.get('matricula')?.value,
+      nombre: this.alumnoForm.get('nombre')?.value,
+      edad: this.alumnoForm.get('edad')?.value,
+      correo: this.alumnoForm.get('correo')?.value,
+      foto: this.alumnoForm.get('foto')?.value,
+      calif: this.alumnoForm.get('calif')?.value
+    };
+    this.alumnos.push(alumno);
+    this.alumnoForm.reset();
+
   }
 
   initForm(): FormGroup {
     return this.fb.group({
       matricula: ['',[Validators.required, Validators.minLength(5)]],
-      nombre: ['',[Validators.required], Validators.minLength(3)],
+      nombre: ['',[Validators.required, Validators.minLength(3)]],
       edad: ['',[Validators.required]],
       correo: ['',[Validators.required]],
       foto: [''],
